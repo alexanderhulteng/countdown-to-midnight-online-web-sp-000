82d464cdd631158522fc058056ef7d8b1c9dce61
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-08-14 01:41:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,4 +6,13 @@ def countdown(int)
     int -= 1
   end
   "HAPPY NEW YEAR!"
+end int > 0
+
+
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
 end
